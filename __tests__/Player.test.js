53041f29e29a72df88fb47790750821832d1ca18
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-09-30 23:17:42 -0500

    tested and created player object while using constructors

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,9 +1,7 @@
-const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
+const Player = require('../lib/Player');
 
 jest.mock('../lib/Potion.js');
-
-console.log(new Potion());
 
 test('creates a player object', () => {
   const player = new Player('Dave');
@@ -13,5 +11,42 @@ test('creates a player object', () => {
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
 
-	expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]))
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+console.log(new Potion());
+
+/*
+When you run npm run test, it will run all files within the __tests__
+directory and any other .test files throughout the project. If you only
+want to run a particular group of related tests (known as a test suite),
+you can modify the npm run command as shown here:
+  npm run test Player
+  npm run test Potion
+*/
+
+/*
+An example of mocking fs (file system module) might look like this:
+
+const fs = require('fs');
+jest.mock('fs');
+fs.readFileSync.mockReturnValue('fake content');
+*/
+
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+  
+  player.inventory = [];
+  expect(player.getInventory()).toEqual(false);
 });
